b58ed5b8a8cc51f39017fee06dcf6a6b312f0fb6
shln53 <shoncoin@gmail.com>
2023-12-01 01:00:29 +0900

    KeySettingMenu 수정

--- src/main/java/kr/ac/jbnu/se/tetris/KeySettingMenu.java
+++ src/main/java/kr/ac/jbnu/se/tetris/KeySettingMenu.java
@@ -6,32 +6,39 @@ import java.awt.event.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class KeySettingMenu extends JPanel {
-    public static AtomicInteger moveLeftKey = new AtomicInteger(KeyEvent.VK_LEFT);
-    public static AtomicInteger moveRightKey = new AtomicInteger(KeyEvent.VK_RIGHT);
-    public static AtomicInteger softDropKey = new AtomicInteger(KeyEvent.VK_DOWN);
-    public static AtomicInteger rotateRightKey = new AtomicInteger(KeyEvent.VK_UP);
-    public static AtomicInteger hardDropKey = new AtomicInteger(KeyEvent.VK_SPACE);
-    public static AtomicInteger holdBlockKey = new AtomicInteger(KeyEvent.VK_C);
-    public static AtomicInteger useItemKey = new AtomicInteger(KeyEvent.VK_I);
-    private JLabel key1, key2, key3, key4, key5, key6, key7;
-    private JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+    public static final AtomicInteger moveLeftKey = new AtomicInteger(KeyEvent.VK_LEFT);
+    public static final AtomicInteger moveRightKey = new AtomicInteger(KeyEvent.VK_RIGHT);
+    public static final AtomicInteger softDropKey = new AtomicInteger(KeyEvent.VK_DOWN);
+    public static final AtomicInteger rotateRightKey = new AtomicInteger(KeyEvent.VK_UP);
+    public static final AtomicInteger hardDropKey = new AtomicInteger(KeyEvent.VK_SPACE);
+    public static final AtomicInteger holdBlockKey = new AtomicInteger(KeyEvent.VK_C);
+    public static final AtomicInteger useItemKey = new AtomicInteger(KeyEvent.VK_I);
+    private JLabel key1;
+    private JLabel key2;
+    private JLabel key3;
+    private JLabel key4;
+    private JLabel key5;
+    private JLabel key6;
+    private JLabel key7;
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
+    private JButton btn4;
+    private JButton btn5;
+    private JButton btn6;
+    private JButton btn7;
     private  JButton previousButton = null;
+    private String buttonFont = "맑은 고딕";
     public KeySettingMenu(Tetris tetris) {
 
         JButton saveButton = new JButton("Close");
         saveButton.setPreferredSize(new Dimension(200, 50));
         saveButton.setMinimumSize(new Dimension(200, 50));
         saveButton.setMaximumSize(new Dimension(200, 50));
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // 설정을 저장하고 설정 메뉴를 닫도록 구현
-                tetris.switchPanel(new SettingMenu(tetris)); // 메인 메뉴로 이동
-            }
-        });
+        saveButton.addActionListener(e -> tetris.switchPanel(new SettingMenu(tetris)));
 
         JLabel keyLabel = new JLabel("조작키 변경", SwingConstants.CENTER);
-        keyLabel.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+        keyLabel.setFont(new Font(buttonFont, Font.BOLD, 18));
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(200, 350));
         JPanel keyPanel = new JPanel();
@@ -67,26 +74,26 @@ public class KeySettingMenu extends JPanel {
     private JButton setStyledButton(JButton button) {
         button.setBackground(new Color(70, 130, 180));
         button.setForeground(Color.WHITE);
-        button.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+        button.setFont(new Font(buttonFont, Font.BOLD, 13));
         button.setFocusPainted(false);
         return button;
     }
 
     private void setKeyLabel() {
         key1 = new JLabel("아이템 사용");
-        key1.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key1.setFont(new Font(buttonFont, Font.BOLD, 12));
         key2 = new JLabel("블록 홀드");
-        key2.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key2.setFont(new Font(buttonFont, Font.BOLD, 12));
         key3 = new JLabel("소프트 드롭");
-        key3.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key3.setFont(new Font(buttonFont, Font.BOLD, 12));
         key4 = new JLabel("하드 드롭");
-        key4.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key4.setFont(new Font(buttonFont, Font.BOLD, 12));
         key5 = new JLabel("왼쪽 이동");
-        key5.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key5.setFont(new Font(buttonFont, Font.BOLD, 12));
         key6 = new JLabel("오른쪽 이동");
-        key6.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key6.setFont(new Font(buttonFont, Font.BOLD, 12));
         key7 = new JLabel("오른쪽 회전");
-        key7.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+        key7.setFont(new Font(buttonFont, Font.BOLD, 12));
     }
 
     private void setKeyButton() {
@@ -109,19 +116,15 @@ public class KeySettingMenu extends JPanel {
     }
 
     private void setButtonListener(JButton buttonName, AtomicInteger key) {
-        buttonName.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (previousButton != null) {
-                    previousButton.setBackground(null);
-                    previousButton.setText(previousButton.getText());
-                }
-                buttonName.setBackground(Color.GRAY);
-                setKeyListener(buttonName, key);
-
-                previousButton = buttonName;
+        buttonName.addActionListener(e -> {
+            if (previousButton != null) {
+                previousButton.setBackground(null);
+                previousButton.setText(previousButton.getText());
             }
+            buttonName.setBackground(Color.GRAY);
+            setKeyListener(buttonName, key);
 
+            previousButton = buttonName;
         });
     }
     private void setKeyListener(JButton buttonName, AtomicInteger key){
